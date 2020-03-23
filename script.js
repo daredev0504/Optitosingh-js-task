df85085a7b369ca12e911a5bf77594da19a4173a
daredev0504 <61061540+daredev0504@users.noreply.github.com>
2020-03-23 14:22:25 +0100

    js added

---
+++ script.js
@@ -0,0 +1,17 @@
+var myName = "Adebimpe Peter Oluwadamilare";
+var courseArray = [];
+courseArray.unshift("html","css","javascript","design");
+console.log(myName);
+console.log(courseArray);
+
+
+function displayEven(){
+    num = 200;
+  for (let i = 0; i <=num; i++) {
+      if (i%2==0) {
+          console.log(i);
+      }
+      
+    }
+}
+displayEven();
